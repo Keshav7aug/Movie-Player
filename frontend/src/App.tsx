328193a99c7b2f3d0f421e43bc35a3931cc44656
Keshav Choudhary <keshav7aug@gmail.com>
2026-07-13 10:06:48 +0530

    added Movie Card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-
+import MovieCard from './components/MovieCard'
 function App() {
 
   return (
     <>
     <Text display="Hello"/>
     <Text display="World"/>
+    <MovieCard title="Peter and Pedro" url="" release_date="2024"></MovieCard>
     </>
   )
 }
---
+++ frontend/src/components/MovieCard.tsx
@@ -0,0 +1,29 @@
+interface Movie {
+    title: string,
+    url: string,
+    release_date: string
+}
+function MovieCard(movie: Movie) {
+
+    function OnFavoriteClikc() {
+        alert("clicked")
+    }
+
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}></img>
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={OnFavoriteClikc}>
+                    ♡
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
+}
+
+export default MovieCard
